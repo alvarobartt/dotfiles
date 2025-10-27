5a48152ff3c738d90ce3ff955f2181eec0f06699
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-10-27 16:53:16 +0900

    Remove annoying `~` for unexisting lines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,6 +11,7 @@ vim.opt.number = true
 -- more is annoying as it takes too much space
 -- less is not recommended given that most of the files might contain > 100 lines
 vim.opt.numberwidth = 3
+vim.opt.fillchars = { eob = " " }
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.undofile = true
